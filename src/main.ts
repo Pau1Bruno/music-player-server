dc212b66e18f8addcef2659eafcc790e3151cdcd
Pau1Bruno <paulbruno@bk.ru>
2023-12-11 19:52:02 +0300

    use cors from library instead Nest cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import * as process from 'process';
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {config} from 'dotenv';
+import cors from "cors";
+
+const options = {
+    origin: "*",
+    credentials: true,
+    methods: ['OPTIONS', 'GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Depth', 'User-Agent', 'X-File-Size', 'X-Requested-With', 'If-Modified-Since', 'X-File-Name', 'Cache-Control'],
+    preflightContinue: true
+}
+
 
 const start = async () => {
     try {
@@ -9,13 +19,7 @@ const start = async () => {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
 
-        app.enableCors({
-            origin: "*",
-            credentials: true,
-            methods: ['OPTIONS', 'GET', 'POST', 'DELETE', 'PUT'],
-            allowedHeaders: ['Content-Type', 'Depth', 'User-Agent', 'X-File-Size', 'X-Requested-With', 'If-Modified-Since', 'X-File-Name', 'Cache-Control'],
-            preflightContinue: true
-        });
+        app.use(cors(options));
 
         await app.listen(PORT, () => {
             console.log(`server starts on PORT ${PORT}`);
